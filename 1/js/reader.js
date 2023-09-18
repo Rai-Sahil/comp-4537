65f5b67504bcbcc85795b31201b51893f8df5322
Sahil Rai <78111534+Rai-Sahil@users.noreply.github.com>
2023-09-17 21:13:54 -0700

    Update reader.js

--- 1/js/reader.js
+++ 1/js/reader.js
@@ -1,28 +1,36 @@
+"use strict";
+
 function updateNotes() {
-  const NOTESDATA = JSON.parse(localStorage.getItem("NOTES"));
+  const NOTES = JSON.parse(localStorage.getItem("NOTES"));
   const NOTE_LIST = document.getElementById("list-notes");
 
   // Clear the existing content in NOTE_LIST
   NOTE_LIST.innerHTML = "";
 
-  for (const note in NOTESDATA) {
-    const notesText = document.createElement("p");
-    notesText.addEventListener("input", (e) => {
-      NOTESDATA[note].bodyText = e.target.value;
+  for (const note in NOTES) {
+    const note_p_tag = document.createElement("p");
+    note_p_tag.addEventListener("input", (e) => {
+      NOTES[note].bodyText = e.target.value;
     });
-    notesText.innerText = NOTESDATA[note].bodyText;
+    note_p_tag.innerText = NOTES[note].bodyText;
 
-    NOTE_LIST.appendChild(notesText);
+    NOTE_LIST.appendChild(note_p_tag);
   }
 }
 
 const updateTime = () => {
   const currentTime = new Date();
-  document.getElementById("time").textContent = ` ${currentTime.getHours()} : ${currentTime.getMinutes()} : ${currentTime.getSeconds()}`;
+  // Create a string in the format dd/mm/yyyy hh:mm:ss
+  const timeString = currentTime.toLocaleString();
+  // Update the HTML element with the current time
+  document.getElementById("time").textContent = `Current Time: ${timeString}`;
 };
 
+// Call the updateNotes function initially
 updateNotes();
+updateTime();
 
+// Set up a timer to call updateNotes every 2 seconds
 setInterval(() => {
   updateNotes();
   updateTime();
